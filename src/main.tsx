05fbf5d6cfa6051296b752641d98e00008e3024e
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-01 14:25:45 +0530

    feat:integrate Sentry error tracking in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,31 @@
+import "./instrument.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import "./index.css";
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById("root")!).render(
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container, {
+  // Callback called when an error is thrown and not caught by an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  // Callback called when React catches an error in an ErrorBoundary.
+  onCaughtError: Sentry.reactErrorHandler(),
+  // Callback called when React automatically recovers from errors.
+  onRecoverableError: Sentry.reactErrorHandler(),
+})
+root.render(
   <StrictMode>
+
     <Provider store={store}>
       <App />
     </Provider>
+
   </StrictMode>,
 );
 
